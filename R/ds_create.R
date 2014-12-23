d84391dca6ded529c9bbf3c59a2f1250acc74949
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-23 10:21:09 -0800

    added ds_create_dataset #4

--- R/ds_create.R
+++ R/ds_create.R
@@ -1,14 +1,26 @@
-#' Datastore - create a datastore
+#' Datastore - create a dataset
 #'
 #' @export
 #'
+#' @param package_id (character) Package ID to add dataset to (required)
+#' @param name (character) Name of the new dataset (required)
+#' @param path (character) Path of the file to add (required)
+#' @param key API key (required)
 #' @template args
 #' @examples \donttest{
-#' ds_create(id='6b30b22e-2bd2-4e3e-b64f-c67d4fb4e3be', url = 'http://demo.ckan.org/')
+#' ds_create_dataset(package_id='testingagain', name="mydata",
+#'                    path="~/github/sac/theplantlist/Actinidiaceae.csv",
+#'                    key=getOption('ckan_demo_key'))
 #' }
 
-ds_create <- function(url = 'http://demo.ckan.org', as='list', ...)
+ds_create_dataset <- function(package_id, name, path, key, url = 'http://demo.ckan.org', as='list', ...)
 {
-  res <- ckan_POST(url, 'datastore_create', body = body, ...)
+  path <- path.expand(path)
+  ext <- strsplit(basename(path), "\\.")[[1]]
+  ext <- ext[length(ext)]
+  body <- list(package_id=package_id, name=name, format=ext, url='upload', upload=upload_file(path))
+  res <- POST(file.path(url, 'api/action/resource_create'), add_headers(Authorization=key), body=body, ...)
+  stop_for_status(res)
+  res <- content(res, "text")
   switch(as, json = res, list = jsl(res), table = jsd(res))
 }
