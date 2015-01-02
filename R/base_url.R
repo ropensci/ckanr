a3c2787c99644859493de778710df77b8dc4ce57
Imanuel Costigan <i.costigan@me.com>
2015-01-03 10:34:23 +1100

    Add base URL setter and getter #7

---
+++ R/base_url.R
@@ -0,0 +1,23 @@
+#' Set the default CKAN URL
+#'
+#' The default CKAN URL is set to the \href{http://data.techno-science.ca}{CSTMC open data portal}
+#' when the \code{ckanr} package is loaded. This base URL is then passed to
+#' \code{ckanr} functions.
+#'
+#' @template args
+#' @return sets the \code{ckanr.default.url} option with \code{\link{options}}.
+#' @examples
+#' \dontrun{
+#' get_ckanr_url()
+#' set_ckanr_url("https://data.gov.au")
+#' get_ckanr_url()
+#' }
+#' @export
+
+set_ckanr_url <- function (url = "http://data.techno-science.ca")
+  options(ckanr.default.url = url)
+
+#' @rdname set_ckanr_url
+#' @export
+
+get_ckanr_url <- function () getOption("ckanr.default.url")
