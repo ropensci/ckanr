913be9cbb7b1a72404680081400c441a23603c8f
Wush Wu <wush978@gmail.com>
2015-04-06 11:57:16 +0800

    implement organization_list

---
+++ R/organization_list.R
@@ -0,0 +1,24 @@
+#' List organization
+#'
+#' @export
+#'
+#' @param order_by character string (only the first element is used).
+#' The field to sort the list by, must be \code{name} or \code{packages}.
+#' @param decreasing logical value. Is the sort-order is decreasing or not.
+#' @param organizations NOT IMPLEMENTED YET
+#' @param all_fields logical value. Return the name or all fields of the object.
+#' @template args
+#' @examples \donttest{
+#' organization_list()
+#' organization_list(as='json')
+#' organization_list(as='table')
+#' }
+organization_list <- function(order_by = c("name", "package"), decreasing = FALSE, organizations = NULL, all_fields = TRUE, url = get_ckanr_url(), as='list', ...)
+{
+  stopifnot(length(order_by) > 0)
+  stopifnot(order_by[1] %in% c("name", "package"))
+  body <- list(sort = sprintf("%s %s", order_by[1], ifelse(decreasing, "dsc", "asc")))
+  if (all_fields) body[["all_fields"]] <- TRUE
+  res <- ckan_POST(url, method='organization_list', body = body, ...)
+  switch(as, json = res, list = jsl(res), table = jsd(res))
+}
