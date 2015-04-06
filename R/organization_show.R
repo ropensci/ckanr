e0d813f8bd70aaca94c82454558af9f663f95e55
Wush Wu <wush978@gmail.com>
2015-04-06 12:17:35 +0800

    implement feature: organization_show

---
+++ R/organization_show.R
@@ -0,0 +1,20 @@
+#' Show an organization
+#'
+#' @export
+#'
+#' @param id (character) Organization id or name.
+#' @param include_datasets (logical). Whether to include a list of the organization’s datasets
+#' @template args
+#' @details By default the help and success slots are dropped, and only the result
+#' slot is returned. You can request raw json with \code{as='json'} then parse yourself
+#' to get the help slot.
+#' @examples \donttest{
+#' }
+organization_show <- function(id, include_datasets = FALSE,
+  url = get_ckanr_url(), as='list', ...)
+{
+  body <- cc(list(id = id))
+  if (include_datasets) body[["include_datasets"]] <- "True"
+  res <- ckan_POST(url, 'organization_show', body = body, ...)
+  switch(as, json = res, list = jsl(res), table = jsd(res))
+}
