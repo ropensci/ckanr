338aedd1eeb318bc5a956fcaf0e8e287c457da46
Wush Wu <wush978@gmail.com>
2015-04-27 08:25:30 +0800

    check the number based on webpage

--- tests/testthat/test-organization_list.R
+++ tests/testthat/test-organization_list.R
@@ -1,12 +1,20 @@
 context("organization_list")
 
+organization_num <- local({
+  res <- httr::GET("http://demo.ckan.org/organization")
+  httr::stop_for_status(res)
+  html <- httr::content(res, as = "text")
+  tmp <- regmatches(html, regexec("(\\d+) organizations found", html))
+  as.integer(tmp[[1]][2])
+})
+
 test_that("organization_list gives back expected class types", {
   a <- organization_list(url = "http://demo.ckan.org")
 
   expect_is(a, "list")
   expect_is(a[[1]], "list")
   expect_is(a[[1]]$state, "character")
-  expect_equal(length(a), 375)
+  expect_equal(length(a), organization_num)
 })
 
 test_that("organization_list works giving back json output", {
@@ -15,6 +23,6 @@ test_that("organization_list works giving back json output", {
   expect_is(b, "character")
   expect_is(b_df, "list")
   expect_is(b_df$result, "data.frame")
-  expect_equal(nrow(b_df$result), 375)
+  expect_equal(nrow(b_df$result), organization_num)
 })
 
