1a114a356f513728913bd07f459b54ea51a2bf62
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-11 12:16:17 -0700

    added two others as authors in pkg level man file

--- R/ckanr-package.R
+++ R/ckanr-package.R
@@ -5,5 +5,7 @@
 #' @docType package
 #' @title R client for the CKAN API
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @author Imanuel Costigan \email{i.costigan@@me.com}
+#' @author Wush Wu
 #' @keywords package
 NULL
