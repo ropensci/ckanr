7c98ce8f4610f2c058cc92ff75bd82bdb6d119c3
florianm <florian.mayer@dpaw.wa.gov.au>
2015-05-13 13:19:38 +0800

    expanded tests for resource_update, introduced new test settings to ckanr_options with getters and setter

--- R/ckanr_options.R
+++ R/ckanr_options.R
@@ -10,15 +10,52 @@
 ckanr_options <- function() {
   ops <- list(url = getOption("ckanr.default.url", NULL),
               key = getOption("X-CKAN-API-Key", NULL),
-              ckan.demo.url = getOption("ckan.demo.url", NULL),
-              ckan.demo.key = getOption("ckan.demo.key", NULL),
-              ckan.demo.resource.id = getOption("ckan.demo.resource.id", NULL)
+              ckanr.test.url = getOption("ckanr.test.url", NULL),
+              ckanr.test.key = getOption("ckanr.test.key", NULL),
+              ckanr.test.rid = getOption("ckanr.test.rid", NULL)
   )
   structure(ops, class = "ckanr_options")
 }
 
 #' @export
 print.ckanr_options <- function(x) {
-  cat(paste0("Base URL: ", x$url), sep = "\n")
-  cat("API key: ", x$key)
+  cat("Base URL: ", x$url, "\n")
+  cat("API key: ", x$key, "\n")
+  cat("Test CKAN URL:", x$ckanr.test.url, "\n")
+  cat("Test CKAN API key:", x$ckanr.test.key, "\n")
+  cat("Test CKAN resource ID:", x$ckanr.test.rid)
 }
+
+
+#'  Set options for CKAN test environment
+#'
+#'  Tests require a valid CKAN URL, a privileged API key for that URL,
+#'  plus the IDs of an existing dataset and an existing resource, repectively.
+#'
+#'  @param url A valid CKAN URL for testing purposes
+#'  @param key A valid CKAN API key privileged to create datasets at URL
+#'  @param did A valid CKAN dataset ID, existing at url
+#'  @param rid A valid CKAN resource ID, attached to did
+#'  @export
+set_test_env <- function(url, key, did, rid) {
+  options(ckanr.test.url = url)
+  options(ckanr.test.key = key)
+  options(ckanr.test.did = did)
+  options(ckanr.test.rid = rid)
+}
+
+#' Get the CKAN test URL
+#' @export
+get_test_url <- function(){getOption("ckan.test.url", NULL)}
+
+#' Get the CKAN test key
+#' @export
+get_test_key <- function(){getOption("ckan.test.key", NULL)}
+
+#' Get the CKAN test dataset ID
+#' @export
+get_test_did <- function(){getOption("ckan.test.did", NULL)}
+
+#' Get the CKAN resource ID
+#' @export
+get_test_rid <- function(){getOption("ckan.test.rid", NULL)}
--- tests/testthat/test-resource_update.R
+++ tests/testthat/test-resource_update.R
@@ -3,39 +3,69 @@ context("resource_update")
 # Use a local example file from package ckanR
 path <- system.file("examples", "actinidiaceae.csv", package = "ckanr")
 
-# Set CKAN connection from global variables - this will vary between testers, run:
-# Sys.setenv("ckan.demo.url"="http://my-ckan.org/", "ckan.demo.key"="my-ckan-key")
-url <- Sys.getenv("ckan.demo.url")
-key <- Sys.getenv("ckan.demo.key")
-rid <- Sys.getenv("ckan.demo.resource.id")
+# Set CKAN connection from ckanr_options
+url <- get_test_url()
+key <- get_test_key()
+rid <- get_test_rid()
 
-# Update resource
-a <- resource_update(id = rid, path=path, url=url, key=key)
+test_that("The CKAN URL must be set", { expect_is(url, "character") })
+test_that("The CKAN API key must be set", { expect_is(key, "character") })
+test_that("A CKAN resource ID must be set", { expect_is(rid, "character") })
 
-# Inspect result
+# Helper functions to test CKAN environment
+check_ckan <- function(){
+  if(!ping(url)) {
+    skip(paste0("CKAN is offline. Tests for resource_update require a live ",
+                "CKAN URL to be set with set_ckanr_url('your_ckan_url')."))
+  }
+}
+
+check_resource <- function(){
+  res <- resource_show(rid, url=url)
+  if(class(res)!="list"){
+   skip(paste0("The CKAN resource doesn't seem to exist. Tests for ",
+               "resource_update require an existing CKAN resource ID to be",
+               "consult ?set_test_env"))
+  }
+}
+
+# Test update_resource
 test_that("resource_update gives back expected class types", {
+  check_ckan()
+  check_resource()
+  a <- resource_update(id = rid, path=path, url=url, key=key)
   expect_is(a, "list")
   expect_is(a$id, "character")
 })
 
 test_that("resource_update gives back expected output", {
-  expect_is(a$id, rid)
+  check_ckan()
+  check_resource()
+  a <- resource_update(id = rid, path=path, url=url, key=key)
+  expect_equal(a$id, rid)
   expect_true(grepl("actinidiaceae", a$url))
 })
 
 test_that("resource_update fails well", {
+  check_ckan()
+  check_resource()
+
   # all parameters missing
   expect_error(resource_update(), "argument \"path\" is missing, with no default")
 
   # bad resource id
-  expect_error(resource_update("invalid-resource-id", path=path, url=url, key=key), "client error")
+  expect_error(resource_update("invalid-resource-id", path=path, url=url, key=key),
+               "client error")
 
   # bad file path: local file does not exist
-  expect_error(resource_update(rid, "invalid-file-path", url=url, key=key), "file does not exist")
+  expect_error(resource_update(rid, "invalid-file-path", url=url, key=key),
+               "file does not exist")
 
   # bad url
-  expect_error(resource_update(rid, path=path, url="invalid-URL", key=key), "Could not resolve host")
+  expect_error(resource_update(rid, path=path, url="invalid-URL", key=key),
+               "Could not resolve host")
 
   # bad key
-  expect_error(resource_update(rid, path=path, url=url, key="invalid-key"), "Forbidden")
+  expect_error(resource_update(rid, path=path, url=url, key="invalid-key"),
+               "Forbidden")
 })
