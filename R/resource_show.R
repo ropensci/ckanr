363fe509c7aa30c82fb1c0e1459519933c0621d9
florianm <florian.mayer@dpaw.wa.gov.au>
2015-05-20 11:01:27 +0800

    removed unused argument "key" from package_show and resource_show to resolve build warning, reflowed comments

--- R/resource_show.R
+++ R/resource_show.R
@@ -8,8 +8,8 @@
 #' resource_show(id = "e179e910-27fb-44f4-a627-99822af49ffa", as="table")
 #' resource_show(id = "05ff2255-c38a-40c9-b657-4ccb55ab2feb", url = "http://data.nhm.ac.uk")
 #' }
-resource_show <- function(id, url = get_ckanr_url(), as = 'list', key = NULL, ...) {
-  res <- ckan_POST(url, 'resource_show', body = list(id = id), key = key, ...)
+resource_show <- function(id, url = get_ckanr_url(), as = 'list', ...) {
+  res <- ckan_POST(url, 'resource_show', body = list(id = id), ...)
   switch(as, json = res, list = jsl(res), table = jsd(res))
 }
 
