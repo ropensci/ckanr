b888d7c690b4571370987bbc6b78dab2dcd922e6
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-28 11:41:25 -0700

    added two functions ckan_info and servers

---
+++ R/ckan_info.R
@@ -0,0 +1,14 @@
+#' Get information on a CKAN server
+#'
+#' @export
+#' @param url Base url to use. Default: \url{http://data.techno-science.ca}. See
+#' also \code{\link{ckanr_setup}} and \code{\link{get_default_url}}. (required)
+#' @param ... Curl args passed on to \code{\link[httr]{GET}} (optional)
+#' @examples \dontrun{
+#' ckan_info()
+#' }
+ckan_info <- function(url = get_default_url(), ...) {
+  res <- GET(file.path(url, "api/util/status"))
+  stop_for_status(res)
+  jsonlite::fromJSON(content(res, "text"))
+}
