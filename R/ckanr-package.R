e7cae3bd8c11a583bd5b625e3273590ff876c596
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-21 12:10:59 -0700

    updated pkg level man file, more explanation of pkg this address, i think, fix #36

--- R/ckanr-package.R
+++ R/ckanr-package.R
@@ -1,4 +1,32 @@
-#' R client for the CKAN API
+#' @title R client for the CKAN API
+#'
+#' @description ckanr is a full client for the CKAN API, wrapping all
+#' APIs, including for reading and writing data. Please get in touch
+#' (\url{https://github.com/ropensci/ckanr/issues}) if you have problems, or
+#' have use cases that we don't cover yet.
+#'
+#' @section CKAN API:
+#'
+#' Document for the CKAN API is at \url{http://docs.ckan.org/en/latest/api/index.html}.
+#' We'll always be following the lastest version of the API.
+#'
+#' @section ckanr package API:
+#'
+#' The functions can be grouped into those for setup, packages,
+#' resources, tags, organizations, and groups.
+#'
+#' \itemize{
+#'  \item Setup - The main one is \code{\link{ckanr_setup}} - and many related
+#'  functions, e.g., \code{\link{get_default_key}}
+#'  \item Packages - Create a package with \code{\link{package_create}}, and see
+#'  other functions starting with \code{package_*}
+#'  \item Resources - Create a package with \code{\link{resource_create}}, and see
+#'  other functions starting with \code{resource_*}
+#'  \item Tags - List tags with \code{\link{tag_list}}, and see
+#'  other functions starting with \code{tag_*}
+#'  \item Organizations - List organizations with \code{\link{organization_list}},
+#'  and show a specific organization with \code{\link{organization_show}}
+#' }
 #'
 #' @importFrom httr GET POST upload_file write_disk add_headers content
 #' stop_for_status http_condition
@@ -6,7 +34,6 @@
 #' @name ckanr-package
 #' @aliases ckanr
 #' @docType package
-#' @title R client for the CKAN API
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @author Imanuel Costigan \email{i.costigan@@me.com}
 #' @author Wush Wu
