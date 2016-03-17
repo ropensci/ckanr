4d7f6316999c0a392f6216018839b3d90e9173fc
Scott Chamberlain <myrmecocystus@gmail.com>
2016-03-17 16:34:44 -0700

    added ckan_Version fxn, bump dev version

--- R/ckan_info.R
+++ R/ckan_info.R
@@ -4,12 +4,29 @@
 #' @param url Base url to use. Default: \url{http://data.techno-science.ca}. See
 #' also \code{\link{ckanr_setup}} and \code{\link{get_default_url}}. (required)
 #' @param ... Curl args passed on to \code{\link[httr]{GET}} (optional)
+#' @return for \code{ckan_info} a list with many slots with various info.
+#' for \code{ckan_version}, list of length two, with actual version as character,
+#' and another with version converted to numeric (any dots or letters removed)
 #' @examples \dontrun{
 #' ckan_info()
 #' ckan_info(servers()[5])
+#'
+#' ckan_version(servers()[5])
 #' }
 ckan_info <- function(url = get_default_url(), ...) {
   res <- httr::GET(file.path(url, "api/util/status"))
   stop_for_status(res)
   jsonlite::fromJSON(httr::content(res, "text", encoding = "UTF-8"))
+}
+
+#' @export
+#' @rdname ckan_info
+ckan_version <- function(x) {
+  ver <- ckan_info(x)$ckan_version
+  nn <- as.numeric(
+    paste0(
+      unlist(regmatches(ver, gregexpr("[[:digit:]]+", ver))),
+      collapse = "")
+  )
+  list(version = ver, version_num = nn)
 }
--- R/package_list.R
+++ R/package_list.R
@@ -14,5 +14,7 @@ package_list <- function(offset = 0, limit = 31, url = get_default_url(),
                          as = 'list', ...) {
   args <- cc(list(offset = offset, limit = limit))
   res <- ckan_GET(url, 'package_list', args, key = NULL, ...)
+  #res <- ckan_GET(url, 'package_list', key = NULL, ...)
   switch(as, json = res, list = jsl(res), table = jsd(res))
 }
+
