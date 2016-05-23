a72e292ce98a8539cefc61309be1055994bd35c6
Scott Chamberlain <myrmecocystus@gmail.com>
2016-05-23 09:32:52 -0700

    fixed changes test, conflicts with changes fxn in dplyr

--- tests/testthat/test-changes.R
+++ tests/testthat/test-changes.R
@@ -4,7 +4,7 @@ u <- get_test_url()
 
 test_that("changes gives back expected class types", {
   check_ckan()
-  a <- changes(url=u)
+  a <- ckanr::changes(url = u)
 
   expect_is(a, "list")
   expect_is(a[[1]], "list")
@@ -14,7 +14,7 @@ test_that("changes gives back expected class types", {
 
 test_that("changes works giving back json output", {
   check_ckan()
-  b <- changes(url=u, as = 'json')
+  b <- ckanr::changes(url = u, as = 'json')
   b_df <- jsonlite::fromJSON(b)
 
   expect_is(b, "character")
@@ -25,8 +25,8 @@ test_that("changes works giving back json output", {
 test_that("changes fails correctly", {
   check_ckan()
 
-  expect_error(changes("adf"), "offset Invalid integer")
-  expect_error(changes(limit = "Adf"), "limit Invalid integer")
-  expect_error(changes("adf", url = "http://www.google.com"),
+  expect_error(ckanr::changes("adf"), "offset Invalid integer")
+  expect_error(ckanr::changes(limit = "Adf"), "limit Invalid integer")
+  expect_error(ckanr::changes("adf", url = "http://www.google.com"),
                regexp = "404")
 })
