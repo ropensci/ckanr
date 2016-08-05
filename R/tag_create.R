e4e7f2b55ac47e58969ebc5f6c7229e35419d162
Scott Chamberlain <myrmecocystus@gmail.com>
2016-08-05 14:46:35 -0700

    #81, removed commented out code inside of tag_create still not been able to test it yet

--- R/tag_create.R
+++ R/tag_create.R
@@ -8,13 +8,15 @@
 #' added to, e.g. the id of vocabulary 'Genre'
 #' @template args
 #' @template key
+#' @examples \dontrun{
+#' tag_create(name = "TestTag1", vocabulary_id = "Testing1")
+#' }
 tag_create <- function(name, vocabulary_id,
                        key = get_default_key(), url = get_default_url(), as = 'list', ...) {
 
-  stop("Not working yet", call. = FALSE)
-  # body <- cc(list(name = name, vocabulary_id = vocabulary_id))
-  # res <- ckan_POST(url, 'tag_create',
-  #                  body = tojun(body, TRUE), key = key,
-  #                  encode = "json", ctj(), ...)
-  # switch(as, json = res, list = jsl(res), table = jsd(res))
+  body <- cc(list(name = name, vocabulary_id = vocabulary_id))
+  res <- ckan_POST(url, 'tag_create',
+                   body = tojun(body, TRUE), key = key,
+                   encode = "json", ctj(), verbose())
+  switch(as, json = res, list = jsl(res), table = jsd(res))
 }
