f9d46ae039cf0ba99c9af325543ea9fe7a0040ac
Florian Mayer <Florian.Mayer@dpaw.wa.govst login: Thu Sep 15 12:05:04 2016 from aws-guacamole-002.corporateict.domain.au>
2017-05-09 21:36:37 +0800

    patch test-group_list to use different CKAN instance

--- tests/testthat/test-group_list.R
+++ tests/testthat/test-group_list.R
@@ -1,5 +1,6 @@
 context("group_list")
-u <- get_test_url()
+# u <- get_test_url()
+u <- "http://www.civicdata.io"
 
 test_that("group_list gives back expected class types", {
   check_ckan(u)
@@ -12,7 +13,7 @@ test_that("group_list gives back expected class types", {
 
 test_that("group_list works giving back json output", {
   check_ckan(u)
-  b <- group_list(url=u, as='json')
+  b <- group_list(url=u, as='json', limit=10)
   b_df <- jsonlite::fromJSON(b)
   expect_is(b, "character")
   expect_is(b_df, "list")
@@ -20,6 +21,6 @@ test_that("group_list works giving back json output", {
 
 test_that("group_list fails correctly", {
   check_ckan(u)
-  expect_error(group_list(sort = "adf", url=u), "Cannot sort by field `adf`")
-  expect_equal(group_list(groups = 4, url=u), list())
+  expect_error(group_list(sort = "adf", url=u, limit=10), "Cannot sort by field `adf`")
+  expect_equal(group_list(groups = 4, url=u, limit=10), list())
 })
