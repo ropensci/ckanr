73cd806610dacf741192d4b2faeea26b818e55c5
Johannes Langer <jogi@rstudioserver.hawadawa.com>
2018-04-25 07:06:00 +0000

    added method functionality to ds_upsert

--- R/ds_upsert.R
+++ R/ds_upsert.R
@@ -14,12 +14,13 @@
 #' @references \url{http://bit.ly/1G9cnBl}
 
 ds_upsert <- function(resource_id = NULL, force = FALSE,
-                      method = 'upsert', records = NULL,
+                      method = c("upsert", "insert", "update"), records = NULL,
                       key = get_default_key(),
                       url = get_default_url(), as = 'list', ...) {
 
+  method <- match.arg(method)
   body <- cc(list(resource_id = resource_id, force = force,
-                  fields = fields, records = records))
+                  records = records, method = method))
   res <- POST(file.path(url, 'api/action/datastore_upsert'),
               add_headers(Authorization = key),
               body = tojun(body, TRUE),
@@ -28,5 +29,3 @@ ds_upsert <- function(resource_id = NULL, force = FALSE,
   res <- content(res, "text", encoding = "UTF-8")
   switch(as, json = res, list = jsl(res), table = jsd(res))
 }
-
-convert <- function(x){ if (!is.null(x)) { jsonlite::toJSON(x) } else { NULL } }
