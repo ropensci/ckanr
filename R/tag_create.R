07b7f4640be7b01ced44c64e3ae59b3402a91227
Scott Chamberlain <myrmecocystus@gmail.com>
2019-07-03 10:31:22 -0700

    fix #81 note in tag_create that only sysadmins can use it update servers() man file

--- R/tag_create.R
+++ R/tag_create.R
@@ -1,18 +1,23 @@
 #' Create a tag
+#' 
+#' IMPORTANT: You must be a sysadmin to create vocabulary tags.
 #'
 #' @export
 #'
 #' @param name (character) The name for the new tag, a string between 2 and 100
-#' characters long containing only alphanumeric characters and -, _ and ., e.g. 'Jazz'
-#' @param vocabulary_id (character) The id of the vocabulary that the new tag should be
-#' added to, e.g. the id of vocabulary 'Genre'
+#' characters long containing only alphanumeric characters and -, _ and .,
+#' e.g. 'Jazz'
+#' @param vocabulary_id (character) The id of the vocabulary that the new
+#' tag should be added to, e.g. the id of vocabulary 'Genre'
 #' @template args
 #' @template key
 #' @examples \dontrun{
+#' ckanr_setup(url = "https://demo.ckan.org/",
+#'   key = Sys.getenv("CKAN_DEMO_KEY"))
 #' tag_create(name = "TestTag1", vocabulary_id = "Testing1")
 #' }
 tag_create <- function(name, vocabulary_id,
-                       key = get_default_key(), url = get_default_url(), as = 'list', ...) {
+  key = get_default_key(), url = get_default_url(), as = 'list', ...) {
 
   warning("not tested yet, may not work", call. = FALSE)
   body <- cc(list(name = name, vocabulary_id = vocabulary_id))
