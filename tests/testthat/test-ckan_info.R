c23b3f1123f99a53b9df506ee8f8732e536c33a4
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-08-03 21:46:47 -0400

    Add tests for parse_version_number

---
+++ tests/testthat/test-ckan_info.R
@@ -0,0 +1,12 @@
+context("test-ckan_info")
+
+test_that("parse_version_number parses correctly.", {
+  expect_equal(parse_version_number("2.1"), 21)
+  expect_equal(parse_version_number("2.1.0"), 21)
+  expect_equal(parse_version_number("2.1.1"), 21.1)
+  expect_equal(parse_version_number("21.1.1"), 211.1)
+  expect_equal(parse_version_number("0.0.1"), 0.1)
+  expect_equal(parse_version_number("0.1.1"), 1.1)
+  expect_equal(parse_version_number("0.1.0"), 1)
+  expect_equal(parse_version_number("0.0.0.9000"), 0.09)
+})
