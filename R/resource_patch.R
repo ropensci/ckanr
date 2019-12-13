2ea42f5fc3c3594b2b8337e8ddd701c69da5c9fe
Nolan Nichols <nnichols@mazetx.com>
2019-12-13 05:05:53 -0800

    doc: full exampled of resource_patch

--- R/resource_patch.R
+++ R/resource_patch.R
@@ -9,16 +9,26 @@
 #' # Setup
 #' ckanr_setup(url = "https://demo.ckan.org", key = getOption("ckan_demo_key"))
 #'
+#' # create a package
+#' (res <- package_create("twist", author="Alexandria"))
+#'
+#' # then create a resource
+#' file <- system.file("examples", "actinidiaceae.csv", package = "ckanr")
+#' (xx <- resource_create(package_id = res$id, description = "my resource"))
+#'
 #' # Get a resource
-#' res <- resource_show("b85948b6-f9ea-4392-805e-00511d6cf6c6")
+#' res <- resource_show(xx$id)
 #' res$description
 #'
 #' # Make some changes
-#' x <- list(description = "My newer description", "extra_key" = "my special value")
-#' resource_patch(x, id = res)
-#' # or pass id in directly
-#' # resource_patch(x, id = res$id)
-#' }
+#' x <- list(description = "My newer description")
+#' z <- resource_patch(x, id = res)
+#' z$description
+#'
+#' # Add an extra key:value pair
+#' extra <- list("extra_key" = "my special value")
+#' zz <- resource_patch(extra, id = res)
+#' zz$extra_key
 resource_patch <- function(x, id, url = get_default_url(),
   key = get_default_key(), as = 'list', ...) {
 
