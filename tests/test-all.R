e07f84efedfa8d680613ba22203e18dd73e67d46
Scott Chamberlain <sckott@fastmail.com>
2021-02-02 18:05:46 -0800

    demo.ckan.org with https

--- tests/test-all.R
+++ tests/test-all.R
@@ -5,6 +5,6 @@ source("testthat/helper-ckanr.R")
 prepare_test_ckan()
 
 # Test CKAN fallback
-if(is.null(get_test_url())) ckanr_setup(test_url="http://demo.ckan.org")
+if(is.null(get_test_url())) ckanr_setup(test_url="https://demo.ckan.org")
 
 test_check("ckanr")
