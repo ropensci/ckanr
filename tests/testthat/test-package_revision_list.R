6eb2a6dd560c4636eb0b1317b1aaf59ffb4891bf
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-03-14 18:13:15 -0300

    Removal of revision_list and package_revision_list in CKAN 2.9 (#200)
    * revision_list and package_revision_list return NULL if CKAN version is greater than 2.9
    
    * add definition of ver variable with ckan version

--- tests/testthat/test-package_revision_list.R
+++ tests/testthat/test-package_revision_list.R
@@ -3,20 +3,30 @@ context("package_revision_list")
 skip_on_cran()
 
 u <- get_test_url()
+ver <- try(ckan_version(u)$version_num, silent = TRUE)
 
-test_that("package_revision_list gives back expected class types", {
-  check_ckan(u)
-  .a <- package_list_current(limit=1, url=u)
-  a <- package_revision_list(.a[[1]]$id, url=u)
-  expect_is(a, "list")
-})
+if (ver >= 29.0) {
+  test_that("removal of package_revision_list endpoint", {
+    check_ckan(u)
+    a <- expect_warning(package_revision_list(url=u), "The ckan.logic.action.get.package_revision_list endpoint was removed in CKAN 2.9. Returning NULL.")
+    expect_null(a)
+  })  
+} else {
+  test_that("package_revision_list gives back expected class types", {
+    check_ckan(u)
+    .a <- package_list_current(limit=1, url=u)
+    a <- package_revision_list(.a[[1]]$id, url=u)
+    expect_is(a, "list")
+  })
+  
+  test_that("package_revision_list works giving back json output", {
+    check_ckan(u)
+    .b <- package_list_current(limit=1, url=u)
+    b <- package_revision_list(.b[[1]]$id, url=u, as="json")
+    b_df <- jsonlite::fromJSON(b)
+    expect_is(b, "character")
+    expect_is(b_df, "list")
+    expect_is(b_df$result, "data.frame")
+  })
+}
 
-test_that("package_revision_list works giving back json output", {
-  check_ckan(u)
-  .b <- package_list_current(limit=1, url=u)
-  b <- package_revision_list(.b[[1]]$id, url=u, as="json")
-  b_df <- jsonlite::fromJSON(b)
-  expect_is(b, "character")
-  expect_is(b_df, "list")
-  expect_is(b_df$result, "data.frame")
-})
