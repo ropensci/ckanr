858ced2fcb31f0810e90ea4877db387b13f2ea4d
Florian Mayer <florian.mayer@dpc.wa.gov.au>
2025-11-06 06:20:38 +0000

    Codespaces builds

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,4 +1,10 @@
 library("testthat")
 library("ckanr")
 
+ckanr::ckanr_setup(
+  test_url = Sys.getenv("CKANR_TEST_URL"),
+  test_key = Sys.getenv("CKANR_TEST_KEY"),
+  test_behaviour = "SKIP"
+)
+
 test_check("ckanr")
