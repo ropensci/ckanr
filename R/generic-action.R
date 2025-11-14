972d7e4f5cd0c29ca5d09bb76e4c6d116412e9c3
Florian Mayer <florian.mayer@dpc.wa.gov.au>
2025-11-14 14:26:18 +0800

    #178 add generic ckan action function (#222)
    Credit: This PR is based on a branch by @sckott 
    
    This PR introduces a generic `ckan_action()` function that provides a flexible interface for making arbitrary CKAN API calls. This allows users to call any CKAN action without needing a specific wrapper function for each one.
    
    **Key Changes:**
    - Added `ckan_action()` function that accepts customizable HTTP verbs, query parameters, request body, and headers
    - Provides a text string return value to accommodate the varying response types from different CKAN actions
    - Includes basic test coverage for success and error cases
    
    ### Reviewed Changes
    
    | File | Description |
    | ---- | ----------- |
    | R/generic-action.R | Implements the generic `ckan_action()` function that delegates to the internal `ckan_VERB()` helper |
    | man/ckan_action.Rd | Generated documentation for the new function with parameter descriptions and usage examples |
    | tests/testthat/test-ckan_action.R | Adds tests for basic functionality and error handling |
    | NAMESPACE | Exports the new `ckan_action()` function to make it available to users |

---
+++ R/generic-action.R
@@ -0,0 +1,35 @@
+#' Generic action function
+#' 
+#' You have to set the CKAN action to use, as well as the HTTP verb,
+#' in addition to any query parameters, request body data, and 
+#' request headers.
+#'
+#' @export
+#' @param action A valid CKAN API action name (e.g., "package_list", "package_show"). See the CKAN API documentation for a full list of actions: https://docs.ckan.org/en/latest/api/index.html
+#' @param query a named list of URL query parameters
+#' @param body data to be used in the body of a request, see 
+#' https://docs.ropensci.org/crul/reference/verb-POST.html for options
+#' @param headers a named list of request headers
+#' @param verb HTTP request verb, e.g., GET, POST
+#' @param url Base url to use. Default: https://data.ontario.ca/ See
+#' also [ckanr_setup()] and [get_default_url()]
+#' @template key
+#' @param ... Curl args passed on to the relevant [crul::HttpClient] method depending on the `verb` parameter (optional)
+#' @return a text string. because we can't know ahead of time for sure
+#' what kind of data will be returned, we return text and the user
+#' can parse as needed.
+#' @examples \dontrun{
+#' ckanr_setup(url = "https://demo.ckan.org/",
+#'  key = getOption("ckan_demo_key"))
+#'
+#' ckan_action("package_list")
+#' ckan_action("package_list", verb = "GET")
+#' ckan_action("package_list", url = 'https://data.nhm.ac.uk')
+#' }
+ckan_action <- function(action, query = NULL, body = NULL,
+  headers = list(), verb = "POST", url = get_default_url(), 
+  key = get_default_key(), ...) {
+
+  ckan_VERB(verb, url, action, body = body, key = key,
+    query = query, headers = headers, opts = list(...))
+}
