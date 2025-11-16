25188c82df40249e424fc3fa8a02b89592dac865
Florian Mayer <florian.mayer@dpc.wa.gov.au>
2025-11-16 12:03:54 +0000

    fix devcontainer

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# set -euo pipefail
 echo "Making docker executable and R library writable for devcontainer user"
 sudo chown -R $(whoami) /var/run/docker* /usr/local/lib/R/site-library
 echo "Installing R dependencies"
