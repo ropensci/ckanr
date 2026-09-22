20761502b6982ad4c56de2265ed6bf7b3da5a55c
Florian Mayer <Florian.Mayer@eagleview.com>
2026-09-22 03:15:43 +0000

    Document ckanext-scheming custom fields in package_patch()
    Custom schema fields go directly in x, not through extras.
    Fixes #233

--- R/package_patch.R
+++ R/package_patch.R
@@ -1,13 +1,19 @@
 #' Update a package's metadata
 #'
+#' If the portal uses the `ckanext-scheming` extension for custom schema
+#' fields, put each custom field directly in `x`. Do not pass custom fields
+#' through `extras`. CKAN rejects custom fields in `extras` with an error.
+#'
 #' @export
-#' @param x (list) A list with key-value pairs
+#' @param x (list) A list with key-value pairs. Put `ckanext-scheming`
+#' custom fields here as named items.
 #' @param id (character) Resource ID to update (optional). If x lacks an "id" field,
 #' this parameter is required.
-#' @param extras (character vector) The dataset's extras
+#' @param extras (list) The dataset's extras
 #' (optional). Extras are arbitrary (key: value) metadata items for datasets.
 #' Each extra dictionary must have keys 'key' (a string) and
-#' 'value' (a string)
+#' 'value' (a string). Use this only for plain CKAN extras, not for
+#' `ckanext-scheming` custom fields.
 #' @param http_method (character) Which HTTP method (verb) to use. Use one of
 #' "GET" or "POST". Default: "GET"
 #' @template args
@@ -26,6 +32,9 @@
 #' # patch
 #' package_patch(res, extras = list(list(key = "foo", value = "bar")))
 #' unclass(package_show(res))
+#'
+#' # Update a ckanext-scheming custom field: put the field in `x`
+#' package_patch(list(id = res$id, internal_notes = "New internal notes"))
 #' }
 package_patch <- function(
   x, id = NULL, extras = NULL, http_method = "GET",
